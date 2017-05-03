23eb7e5fa0b0b0a7eeaca2171a5606ef6d2dd01b
hfoffani <hfoffani@gmail.com>
2017-05-03 18:21:45 +0200

    Add option args.

--- java/src/main/java/Main.java
+++ java/src/main/java/Main.java
@@ -1,19 +1,38 @@
 
 import java.io.*;
+import java.util.*;
+
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.JCommander;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        if (args.length != 1) {
-            System.err.println("Need input file.");
-            return;
-        }
+    @Parameter(names = "-verbose", description = "Shows commands line by line")
+    boolean verbose = false;
+
+    @Parameter(names = "-driver", description = "Path to chromedriver")
+    String driverpath = "./chromedriver";
+
+    @Parameter(names = "-input", description = "Input file name")
+    String input = "-";
+
+    public static void main(String[] argv) throws Exception {
+
+        Main main = new Main();
+        JCommander.newBuilder()
+                .addObject(main)
+                .build()
+                .parse(argv);
+        main.run();
+    }
+
+    public void run() throws Exception {
+
         WebProfiler wp = new WebProfiler();
         wp.setUp();
         try {
-            String fileName = args[0];
-            if (!fileName.equals("-")) {
-                System.setIn(new FileInputStream(new File(fileName)));
+            if (!input.equals("-")) {
+                System.setIn(new FileInputStream(new File(input)));
             }
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             br.lines()
@@ -24,3 +43,4 @@ public class Main {
         }
     }
 }
+
