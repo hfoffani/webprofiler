1edbfc5647b83a1e5419e02b63afd550958dab95
hfoffani <hfoffani@gmail.com>
2017-05-04 10:41:50 +0200

    Fix bug.

--- docker/webprofiler.sh
+++ docker/webprofiler.sh
@@ -6,8 +6,7 @@ else
     CMDFILE=$1
 fi
 
-CMDFILE=$1
 JARFILE=web-profiler-1.0-jar-with-dependencies.jar
 
 cd /headless/webprofiler
-java -jar $JARFILE -driver /usr/bin/chromedriver -input $1
+java -jar $JARFILE -driver /usr/bin/chromedriver -input $CMDFILE
