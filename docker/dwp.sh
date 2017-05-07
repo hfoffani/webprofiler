854cca17ffa487d462db7f758168a940b4c4e500
hfoffani <hfoffani@gmail.com>
2017-05-07 13:12:39 +0200

    Tools for the new smaller image.

--- docker/dwp.sh
+++ docker/dwp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-FS=`pwd`/../scripts
-# docker run -v $FS:/data herchu/webprofiler /headless/webprofiler/webprofiler.sh /data/w3c.tst
-docker run herchu/webprofiler /headless/webprofiler/webprofiler.sh
+# FS=`pwd`/../scripts
+# docker run -v $FS:/data herchu/webprofiler /home/headless/webprofiler.sh /data/w3c.tst
+docker run herchu/webprofiler /home/headless/webprofiler.sh
