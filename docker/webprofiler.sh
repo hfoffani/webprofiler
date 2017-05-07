c935ebac0da4c66c022c2e6d2b8e339302c61fb9
hfoffani <hfoffani@gmail.com>
2017-05-07 13:00:42 +0200

    A smaller docker image with the same functionality.

--- docker/webprofiler.sh
+++ docker/webprofiler.sh
@@ -8,5 +8,14 @@ fi
 
 JARFILE=web-profiler-1.0-jar-with-dependencies.jar
 
-cd /headless/webprofiler
+export DISPLAY=:99
+/etc/init.d/xvfb start
+sleep 1
+
+cd /home/headless
 java -jar $JARFILE -driver /usr/bin/chromedriver -input $CMDFILE
+
+exit_value=$?
+/etc/init.d/xvfb stop
+exit $exit_value
+
